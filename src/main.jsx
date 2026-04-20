61b89c25de3b9f879c1bb4c0ba953dc529ad3e37
Dhruv-Brahmbhatt <dhruv.25bcs10324@sst.scaler.com>
2026-04-20 18:54:15 +0530

    Set BrowserRouter basename for GitHub Pages subpath

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/YUKAS_ENDTerm">
       <AuthProvider>
         <App />
         <Toaster
